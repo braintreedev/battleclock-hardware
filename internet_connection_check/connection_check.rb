a49acd2923319c9af82bec94c209cdacccbaabcb
Cristiano Betta <cbetta@gmail.com>
2014-09-20 21:22:13 +0100

    Daemonized the code

--- internet_connection_check/connection_check.rb
+++ internet_connection_check/connection_check.rb
@@ -1,18 +1,16 @@
 require 'net/http'
 
-
 def check_server server, port
-  begin 
+  begin
     http = Net::HTTP.start(server, port, {open_timeout: 5, read_timeout: 5})
     response = http.head("/")
-  
-
   rescue
     http = Net::HTTP.start("127.0.0.1", 5555)
     http.get('/arduino/alarm')
-
   end
 end
 
-check_server "nu.nl", 80
-
+while true
+  check_server "google.com", 80
+  sleep(1)
+end
